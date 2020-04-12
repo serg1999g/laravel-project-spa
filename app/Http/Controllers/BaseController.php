38cf89a0b33ee51843cd186d1a0037d601d74de0
serg1999g <46674079+serg1999g@users.noreply.github.com>
2020-04-12 20:04:33 +0300

    Connect Laravel passport. Created BaseApiController. Created RegisterController

---
+++ app/Http/Controllers/BaseController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller as Controller;
+
+class BaseController extends Controller
+{
+    /**
+     * success response method.
+     *
+     * @param $result
+     * @param $message
+     * @return \Illuminate\Http\Response
+     */
+    public function sendResponse($result, $message)
+    {
+        $response = [
+            'success' => true,
+            'data' => $result,
+            'message' => $message,
+        ];
+        return response()->json($response, 200);
+    }
+
+    /**
+     * return error response.
+     *
+     * @param $error
+     * @param $errorMessages
+     * @param $code
+     * @return \Illuminate\Http\Response
+     */
+    public function sendError($error, $errorMessages = [], $code = 404)
+    {
+        $response = [
+            'success' => false,
+            'message' => $error,
+        ];
+        if (!empty($errorMessages)) {
+            $response['data'] = $errorMessages;
+        }
+        return response()->json($response, $code);
+    }
+}
---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\BaseController as BaseController;
+use App\Http\Requests\RegisterRequests;
+use App\User;
+use Illuminate\Http\Request;
+
+
+class RegisterController extends BaseController
+{
+
+    /**
+     * Register api
+     *
+     * @param $request
+     * @return \Illuminate\Http\Response
+     */
+    public function register(RegisterRequests $request)
+    {
+        $user = User::create([
+            'name' => $request->input('name'),
+            'email' => $request->input('email'),
+            'password' => bcrypt($request->input('password')),
+        ]);
+
+        $success['token'] = $user->createToken('Token Name')->accessToken;
+        $success['name'] = $user->name;
+
+        return $this->sendResponse($success, 'User register successfully.');
+    }
+}
