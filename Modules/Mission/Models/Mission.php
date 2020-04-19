058104054b9144c534dd36d94aa585d1916639a4
serg1999g <46674079+serg1999g@users.noreply.github.com>
2020-04-19 21:57:16 +0300

    Refactoring Mission model

--- Modules/Mission/Models/Mission.php
+++ Modules/Mission/Models/Mission.php
@@ -12,9 +12,4 @@ class Mission extends Model
     protected $fillable = [
         'name', 'description'
     ];
-
-    public function user()
-    {
-        return $this->belongsTo(User::class);
-    }
 }
